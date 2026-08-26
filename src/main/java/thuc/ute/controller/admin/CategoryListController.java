c85b52e0062a0773fea3aceb701fe7380fbdeb6d
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-26 23:47:30 +0700

    Ex2: Create Admin CategoryListController

--- src/main/java/thuc/ute/controller/admin/CategoryListController.java
+++ src/main/java/thuc/ute/controller/admin/CategoryListController.java
@@ -1,11 +1,37 @@
 package thuc.ute.controller.admin;
 
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/admin/categories"})
-public class CategoryListController extends HttpServlet{
+import thuc.ute.models.CategoryModel;
+import thuc.ute.services.ICategoryService;
+import thuc.ute.services.impl.CategoryServiceImpl;
 
-	private static final long serialVersionUID = -9150238504900588025L;
+@WebServlet(urlPatterns = { "/admin/category/list" })
+public class CategoryListController extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
+	ICategoryService cateService = new CategoryServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		List<CategoryModel> cateList = cateService.getAll();
+
+		req.setAttribute("cateList", cateList);
+
+		RequestDispatcher dispatcher =
+				req.getRequestDispatcher("/views/admin/list-category.jsp");
+
+		dispatcher.forward(req, resp);
+	}
 }
