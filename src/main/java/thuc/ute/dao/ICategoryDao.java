c44fae88a6ec09e46343457faf3397be9067d1e2
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-08-26 23:15:20 +0700

    Ex2: Create Interface Category and DAO

---
+++ src/main/java/thuc/ute/dao/ICategoryDao.java
@@ -0,0 +1,22 @@
+package thuc.ute.dao;
+
+import java.util.List;
+
+import thuc.ute.models.CategoryModel;
+
+public interface ICategoryDao {
+
+	void insert(CategoryModel category);
+
+	void edit(CategoryModel category);
+
+	void delete(int id);
+
+	CategoryModel get(int id);
+
+	CategoryModel get(String name);
+
+	List<CategoryModel> getAll();
+
+	List<CategoryModel> search(String keyword);
+}
